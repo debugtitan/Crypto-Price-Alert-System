f5e716b4bb77650841e6d7c7df3b2951e6ed8abe
debugtitan <alts.devs@gmail.com>
2024-08-10 22:20:37 +0100

    config module

---
+++ config/__init__.py
@@ -0,0 +1,14 @@
+from __future__ import absolute_import
+
+import os
+import sys
+import environ
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
+sys.path.append(str(BASE_DIR / "core"))
+env = environ.Env()
+env_path = os.path.join(BASE_DIR, ".env")
+
+if os.path.exists(env_path):
+    environ.Env.read_env(env_path, overwrite=True)
