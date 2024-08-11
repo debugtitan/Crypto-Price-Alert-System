0bc0545c3826478f7c2b77f679fd7201ce3f5e41
debugtitan <alts.devs@gmail.com>
2024-08-11 08:00:02 +0100

    celery queue app

---
+++ config/celery/queue.py
@@ -0,0 +1,15 @@
+from kombu import Queue
+
+
+class CeleryQueue:
+    class Definitions:
+        BEATS = "beats"
+
+    @staticmethod
+    def queues():
+        return tuple(
+            (Queue(getattr(CeleryQueue.Definitions, item)))
+            for item in filter(
+                lambda ref: not ref.startswith("_"), dir(CeleryQueue.Definitions)
+            )
+        )
